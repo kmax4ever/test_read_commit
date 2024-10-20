bb1f5f5edc562759d1e640d856a212d266e15c23
kmax <115686499+kmax4ever@users.noreply.github.com>
2024-10-20 09:20:52 +0700

    Add files via upload
    test commitiii1111

---
+++ test111.js
@@ -0,0 +1,6 @@
+// const formatDateToYyMmDd = (date) => {
+//   const yy = String(date.getFullYear()); // Get last 2 digits of the year
+//   const mm = String(date.getMonth() + 1).padStart(2, "0"); // Get month (1-12) and pad with zero
+//   const dd = String(date.getDate()).padStart(2, "0"); // Get day and pad with zero
+//   return `${yy}-${mm}-${dd}`; // Return the formatted string
+// };
